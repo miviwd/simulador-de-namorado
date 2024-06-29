7395e2f1ecacb949a8c9f61473c21d4cc23427fd
Emilly Victória <127414606+miviwd@users.noreply.github.com>
2024-06-29 18:12:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        window.location.href = 'game.html';
+    }, 5000); // 5000 milissegundos = 5 segundos
+});
